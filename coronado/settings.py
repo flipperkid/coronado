7fd3d3fa2b4ee36183bcd9598f064cb03629910b
Chris Jordan <flipperkid@gmail.com>
2014-08-31 18:12:17 -0400

    use 127.0.0.1

--- coronado/settings.py
+++ coronado/settings.py
@@ -64,7 +64,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'coronado',
         'USER': 'postgres',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '5432'
     }
 }
